89e1981c060352e68b36110752eaea53a968e9fa
Tamicktom <henrique.angelo@enterscience.com.br>
2024-05-18 13:16:02 -0300

    processamento-de-cadeias

---
+++ components/Cadeia.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Position from "./Position.vue";
+
+const props = defineProps({
+  cadeia: {
+    type: String,
+    default: "",
+  },
+})
+
+const cadeia = ref(props.cadeia);
+</script>
+
+<template>
+  <div class="items-center justify-center flex">
+    <Position
+      v-for="(char, index) in cadeia"
+      :key="index"
+      :count="index"
+      :string="char"
+    />
+  </div>
+</template>
---
+++ components/Position.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps({
+  count: {
+    default: 0,
+  },
+  string: {
+    type: String,
+    default: ' ',
+  },
+  success: {
+    type: Boolean,
+    default: false,
+  },
+  failure: {
+    type: Boolean,
+    default: false,
+  },
+  locked: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const counter = ref(props.count);
+const string = ref(props.string);
+const success = ref(props.success);
+const failure = ref(props.failure);
+const locked = ref(props.locked);
+
+function handleLeftPointerDown(pointerEvent: PointerEvent) {
+  console.log("pointer down", pointerEvent);
+
+  const isCtrl = pointerEvent.ctrlKey;
+  const isShift = pointerEvent.shiftKey;
+  const isAlt = pointerEvent.altKey;
+  const irRight = pointerEvent.button === 2;
+
+  //if isCtrl
+  if (isCtrl) {
+    success.value = true;
+    failure.value = false;
+    locked.value = false;
+    return;
+  }
+
+  //if isShift
+  if (isShift) {
+    success.value = false;
+    failure.value = true;
+    locked.value = false;
+    return;
+  }
+
+  //if isAlt
+  if (isAlt) {
+    success.value = false;
+    failure.value = false;
+    locked.value = true;
+    return;
+  }
+
+  //if right click
+  if (irRight) {
+    success.value = false;
+    failure.value = false;
+    locked.value = false;
+    return;
+  }
+}
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center">
+    <button class="flex flex-col items-center justify-center" @pointerdown="handleLeftPointerDown">
+      <div class="w-8">
+        <span class="font-mono text-xs font-bold">{{ counter }}</span>
+      </div>
+      <div
+        class="size-8 border bg-neutral-200 border-neutral-600/30 data-[success=true]:bg-emerald-200 data-[failure=true]:bg-rose-200 transition-all duration-300 ease-in-out data-[success=true]:border-emerald-600/30 data-[failure=true]:border-rose-600/30 data-[locked=true]:bg-neutral-400 data-[locked=true]:border-neutral-800"
+        :data-success="success" :data-failure="failure" :data-locked="locked">
+        <span class="text-lg font-bold text-neutral-900">{{ string }}</span>
+      </div>
+    </button>
+  </div>
+</template>
